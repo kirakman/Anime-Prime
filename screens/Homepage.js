05d18385e0f4c62e203668bee47e3efdd4ee429f
kirakman <dourado178@gmail.com>
2024-03-26 20:32:30 -0300

    background into homepage

--- screens/Homepage.js
+++ screens/Homepage.js
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, ImageBackground, StyleSheet } from 'react-native'
 import React from 'react'
 
 const Homepage = () => {
   return (
-    <View>
-      <Text>Homepage</Text>
-    </View>
+  <ImageBackground
+  source={require('../assets/Backgrounds/Homepage.png')}
+  style={styles.backgroundImage}>
+    <SafeAreaView>
+      <View>
+        <Text>Homepage</Text>
+      </View>
+      </SafeAreaView>
+  </ImageBackground>
   )
 }
 
-export default Homepage
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  backgroundImage: {
+      flex: 1,
+      width: '100%', 
+      height: '100%', 
+      resizeMode: 'cover', 
+  }
+});
+
+export default Homepage;
