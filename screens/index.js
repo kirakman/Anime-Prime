5ac784bccafee0157729352b09e4e8e2d07180e3
kirakman <dourado178@gmail.com>
2024-03-31 12:38:36 -0300

    create profile component

--- screens/index.js
+++ screens/index.js
@@ -2,10 +2,12 @@ import Homepage from "./Homepage";
 import Login from "./Login"; 
 import Register from "./Register";
 import Dashboard from "./Dashboard";
+import Profile  from "./Profile";
 
 export {
     Homepage,
     Login,
     Register,
-    Dashboard
+    Dashboard,
+    Profile
 }
